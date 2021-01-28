af8972aaa39d21cd92ece3d82d65799a230610cd
second <zhl614818354@163.com>
2021-01-29 00:30:14 +0800

    day10提交一次

---
+++ day10/src/com/offcn/servlet/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.offcn.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class LoginServlet implements Servlet{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("username");
+		String pwd  = request.getParameter("password");
+		System.out.println("’À∫≈"+name+"√‹¬Î£¨"+pwd);
+	}
+
+}
